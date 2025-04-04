49d0cc4092612391a7a8433f55598ec3fdf5d820
lorenzo-nascimento <lorenzonascimento.dev@gmail.com>
2025-04-04 13:30:56 -0300

    feat: adiciona validação e mapeamento JPA entre Cliente e Pedido

--- src/main/java/br/lorenzo/ms_pedidos/model/Cliente.java
+++ src/main/java/br/lorenzo/ms_pedidos/model/Cliente.java
@@ -1,14 +1,37 @@
 package br.lorenzo.ms_pedidos.model;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.util.ArrayList;
 import java.util.List;
 
 
+@Entity
+@Getter @Setter
+@NoArgsConstructor
+@Table(name = "clientes_tb")
 public class Cliente {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
+    @NotBlank(message = "nome é obrigatório")
+    @Size(min = 3, max = 100, message = "Nome deve ter entre 3 a 100 caracteres")
     private String nome;
+
+    @NotBlank(message = "email é obrigatório")
+    @Email(message = "email deve ser válido")
+    @Column(unique = true)
     private String email;
-    private List<Pedido> pedidos = new ArrayList<>()
+
+    @OneToMany(mappedBy = "cliente", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Pedido> pedidos = new ArrayList<>();
 
 }
--- src/main/java/br/lorenzo/ms_pedidos/model/Pedido.java
+++ src/main/java/br/lorenzo/ms_pedidos/model/Pedido.java
@@ -1,18 +1,36 @@
 package br.lorenzo.ms_pedidos.model;
 
 import br.lorenzo.ms_pedidos.model.enums.StatusPedido;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.cglib.core.Local;
 
 import java.time.LocalDateTime;
 
-
+@Entity
+@Getter @Setter
+@NoArgsConstructor
+@Table(name = "pedidos_tb")
 public class Pedido {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
     private StatusPedido status;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "cliente_id", nullable = false)
     private Cliente cliente;
+
+    @Column(name = "data_criacao", updatable = false)
     private LocalDateTime dataCriacao = LocalDateTime.now();
 
+    @PrePersist
     protected void onCreate() {
         this.dataCriacao = LocalDateTime.now();
     }
