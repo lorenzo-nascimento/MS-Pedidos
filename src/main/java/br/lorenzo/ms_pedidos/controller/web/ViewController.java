095fda377eb3cda64cc79fc9ac4e9c4567ad8663
lorenzo-nascimento <lorenzonascimento.dev@gmail.com>
2025-04-07 18:41:45 -0300

    feat: adiciona listagem de pedidos por cliente e ação de alteração de status

--- src/main/java/br/lorenzo/ms_pedidos/controller/web/ViewController.java
+++ src/main/java/br/lorenzo/ms_pedidos/controller/web/ViewController.java
@@ -1,5 +1,6 @@
 package br.lorenzo.ms_pedidos.controller.web;
 
+import br.lorenzo.ms_pedidos.model.enums.StatusPedido;
 import br.lorenzo.ms_pedidos.service.ClienteService;
 import br.lorenzo.ms_pedidos.service.PedidoService;
 import org.springframework.stereotype.Controller;
@@ -7,6 +8,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/")
@@ -41,6 +44,21 @@ public class ViewController {
     public String listarPedidosPorCliente(@PathVariable Long id, Model model) {
         model.addAttribute("pedidos", pedidoService.listarPedidosPorCliente(id));
         model.addAttribute("cliente", clienteService.buscarPorId(id));
-        return "pedidos-cliente";
+        return "pedidos-cliente"; // Nome do template sem extensão
     }
+
+    @GetMapping("/pedidos/{id}/status")
+    public String atualizarStatusPedido(
+            @PathVariable Long id,
+            @RequestParam String novoStatus,
+            RedirectAttributes redirectAttributes) {
+
+        StatusPedido status = StatusPedido.valueOf(novoStatus);
+        pedidoService.atualizarStatus(id, status);
+
+        redirectAttributes.addFlashAttribute("mensagem", "Status atualizado para " + novoStatus);
+        return "redirect:/pedidos";
+    }
+
+
 }
